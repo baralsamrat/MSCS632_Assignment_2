1af624cafbaf316616d2bfb1c0c5614ca986e1e1
Samrat Baral <sbaral30064@ucumberlands.edu>
2025-03-16 08:45:18 -0400

    Create 3.rs

---
+++ src/3/3.rs
@@ -0,0 +1,7 @@
+// Rust: Using Box<T> for heap allocation
+fn main() {
+    // 'Box' allocates memory on the heap and transfers ownership to variable 'x'
+    let x = Box::new(42);
+    println!("The value of x is: {}", x);
+    // Memory is automatically freed when 'x' goes out of scope.
+}
