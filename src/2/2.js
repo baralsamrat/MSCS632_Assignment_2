1d264b849c4f795978a76904d772479e3027d57e
Samrat Baral <sbaral30064@ucumberlands.edu>
2025-03-16 08:41:46 -0400

    Create 2.js

---
+++ src/2/2.js
@@ -0,0 +1,14 @@
+// JavaScript: Demonstrating lexical scoping and closures
+function outer() {
+    let x = 10;
+    function inner() {
+        // JavaScript functions create closures capturing variables from the outer scope.
+        x += 1;
+        return x;
+    }
+    return inner;
+}
+
+const closureFunction = outer();
+console.log("JavaScript closure call 1:", closureFunction());  // 11
+console.log("JavaScript closure call 2:", closureFunction());  // 12
