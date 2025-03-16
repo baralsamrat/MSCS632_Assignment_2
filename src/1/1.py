150c20e114c9eca93957768b95a69f79592e9b6b
Samrat Baral <sbaral30064@ucumberlands.edu>
2025-03-16 09:28:09 -0400

    Update 1.py

--- src/1/1.py
+++ src/1/1.py
@@ -1,4 +1,3 @@
-# Intentional error: using letter 'o' instead of digit 0 and a missing colon in the function definition
 def calculate_sum(arr):  # Missing colon here
     total = 0
     for num in arr:
@@ -8,3 +7,6 @@ def calculate_sum(arr):  # Missing colon here
 numbers = [1, 2, 3, 4, 5]
 result = calculate_sum(numbers)
 print("Sum in Python:", result)
+
+# Output
+# Sum in Python: 15
