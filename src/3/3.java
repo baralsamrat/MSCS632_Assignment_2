e77c2a2f4507874e99704cc106e35ef9d6247e8f
Samrat Baral <sbaral30064@ucumberlands.edu>
2025-03-16 09:55:49 -0400

    Update 3.java

--- src/3/3.java
+++ src/3/3.java
@@ -4,9 +4,14 @@ public class MemoryDemo {
         // Allocate an array of Integer objects
         Integer[] numbers = new Integer[5];
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = new Integer(i);
+            // Autoboxing automatically converts int to Integer
+            numbers[i] = i;
         }
         System.out.println("Numbers allocated in Java.");
         // The garbage collector automatically frees memory when objects are no longer referenced.
     }
 }
+
+// Output
+// Numbers allocated in Java.
+
