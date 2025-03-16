4281c8c3a4dae6527a29d0630ef6d9f0198ffeb1
Samrat Baral <sbaral30064@ucumberlands.edu>
2025-03-16 10:04:41 -0400

    Update 2.js

--- src/2/2.js
+++ src/2/2.js
@@ -12,3 +12,8 @@ function outer() {
 const closureFunction = outer();
 console.log("JavaScript closure call 1:", closureFunction());  // 11
 console.log("JavaScript closure call 2:", closureFunction());  // 12
+
+
+// Ouput
+// JavaScript closure call 1: 11
+// JavaScript closure call 2: 12
