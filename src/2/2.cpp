7eae02adc103f4978068f87d1bb354aaa32f1d7d
Samrat Baral <sbaral30064@ucumberlands.edu>
2025-03-16 08:42:16 -0400

    Create 2.cpp

---
+++ src/2/2.cpp
@@ -0,0 +1,16 @@
+// C++: Demonstrating static typing and lambda closures
+#include <iostream>
+#include <functional>
+using namespace std;
+
+int main() {
+    int x = 10;
+    // C++ lambda capturing 'x' by value and marked mutable to allow modification.
+    auto inner = [x](int add) mutable -> int {
+        x += add;
+        return x;
+    };
+    cout << "C++ lambda call 1: " << inner(1) << endl;  // 11
+    cout << "C++ lambda call 2: " << inner(1) << endl;  // 12 (x is updated within the lambda)
+    return 0;
+}
